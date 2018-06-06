fa62d25024e6b0a0a54fa5b3a885e077553ece4d
BuildTools <jromphf@library.rochester.edu>
2018-06-05 20:25:38 -0400

    turned this into an installable module

---
+++ dhsi_multimedia/ia_examples/download_items.py
@@ -0,0 +1,12 @@
+# utils
+from io_utils import IOParser
+from ia_helper import download_files, FORMAT_OPTION, parse_formats
+
+if __name__ == '__main__':
+    parser = IOParser(add_args=[FORMAT_OPTION])
+    query, outdir, formats = parser.get_all_arguments()
+    paths = []
+    if formats:
+        paths = download_files(query, outdir, formats=parse_formats(formats))
+    else:
+        paths = download_files(query, outdir)
