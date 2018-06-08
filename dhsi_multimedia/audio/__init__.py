464a9b15d7ee7f8d22030935b465331b055b0b66
BuildTools <jromphf@library.rochester.edu>
2018-06-08 14:20:57 -0400

    fixed audio averaging

--- dhsi_multimedia/audio/__init__.py
+++ dhsi_multimedia/audio/__init__.py
@@ -35,7 +35,7 @@ def avg_audio(paths, out, duration):
         track = track[index:index+chunk]
         avg = avg + track / num_tracks
 
-    wavfile.write(np.normaout, rate, avg)
+    wavfile.write(out, rate, avg)
 
 DURATION = {
     'short': '-d',
